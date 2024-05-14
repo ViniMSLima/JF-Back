9a55bff5edcdff83f53931757ec70ef26ce8e544
ViniMSLima <vinisary.lima@gmail.com>
2024-05-14 08:45:35 -0300

    trying to fix timeout

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const app = express();
 // MongoDB connection
 async function connectToDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_CONNECT_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect("mongodb+srv://vinisarylima:aMuFJSCjBV6L4s8C@cluster0.xm12ebk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("Connected to MongoDB Atlas / Compass");
   } catch (error) {
     console.error("Error connecting to MongoDB Atlas / Compass:", error);
